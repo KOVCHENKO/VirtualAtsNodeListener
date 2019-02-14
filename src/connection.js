20e59f3119764a6a2561d180066c7efa3da55c22
il-kow@mail.ru <il-kow@mail.ru>
2019-02-14 10:56:48 +0300

    feat(Little Folders Structure)

--- src/connection.js
+++ src/connection.js
@@ -1,17 +1,15 @@
-// var server = require('../bin/www');
-//
-// var io = require('socket.io')(server);
-//
-// io.on('connection', function(socket) {
-//
-//     console.log('new connection has been made');
-//
-//     var connectionName = socket.handshake.query['x-clientid'];
-//
-//     socket.on('disconnect', function () {
-//
-//
-//     });
-// });
-//
-// module.exports = io;
+module.exports = function(io){
+
+    io.on('connection', function(socket) {
+
+        console.log('new connection has been made');
+
+        var connectionName = socket.handshake.query['x-clientid'];
+
+        socket.on('disconnect', function () {
+
+
+        });
+    });
+
+};
