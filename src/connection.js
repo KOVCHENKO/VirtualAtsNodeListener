a3a671530ec777d9ed7522249875a3cd55a8907a
il-kow@mail.ru <il-kow@mail.ru>
2019-02-14 10:13:28 +0300

    init

---
+++ src/connection.js
@@ -0,0 +1,17 @@
+// var server = require('../bin/www');
+//
+// var io = require('socket.io')(server);
+//
+// io.on('connection', function(socket) {
+//
+//     console.log('new connection has been made');
+//
+//     var connectionName = socket.handshake.query['x-clientid'];
+//
+//     socket.on('disconnect', function () {
+//
+//
+//     });
+// });
+//
+// module.exports = io;
