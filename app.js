36bc0af11fc51bf504ac2ccea73182ca4acf4616
ILKOV <il-kow@mail.ru>
2019-02-15 10:05:51 +0300

    feat(logging)

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var callsRouter = require('./routes/calls');
 
 var app = express();
@@ -16,10 +15,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/calls', callsRouter);
 
 
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 module.exports = app;
--- routes/calls.js
+++ routes/calls.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET incoming call. */
-router.post('/', function(req, res, next) {
+router.post('/receive', function(req, res, next) {
 
-    console.log(req.body);
+    console.log(req.body.surname);
 
-    req.app.io.emit('new_claim', {key:"value"});
+    // logging of file
+    const fs = require('fs');
+    fs.writeFile("./src/logs/call.log", JSON.stringify(req.body), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+
+    req.app.io.emit('new_call', {key:"value"});
 
     res.send('new call emitter has been send');
 
